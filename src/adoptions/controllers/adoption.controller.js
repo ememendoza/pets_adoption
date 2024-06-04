8e3251239acc621db4fad3c8cc669f7654918af3
ememendozar <ememendozar002@gmail.com>
2024-06-04 12:24:04 -0500

    Feat: Add get and post for adoption

--- src/adoptions/controllers/adoption.controller.js
+++ src/adoptions/controllers/adoption.controller.js
@@ -1,4 +1,36 @@
 import getConnection from "../database/database.js";
 import Adoption from "../models/adoption.js";
 
-export const methods = {};
+const getAdoption = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT * FROM adoption");
+    res.json(result);
+  } catch (error) {
+    res.status(500);
+    res.send(error.message);
+  }
+};
+
+const postAdoption = async (req, res) => {
+  try {
+    const { adoptionDate, adoptionType, adoptionDuration, adoptionConditions } =
+      req.body;
+    const adoption = new Adoption(
+      adoptionDate,
+      adoptionType,
+      adoptionDuration,
+      adoptionConditions
+    );
+    const connection = await getConnection();
+    const result = await connection.query(
+      "INSERT INTO adoption SET ?",
+      adoption
+    );
+    res.json(result);
+  } catch (error) {}
+};
+export const methods = {
+  getAdoption,
+  postAdoption,
+};
--- src/adoptions/routes/adoption.route.js
+++ src/adoptions/routes/adoption.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { methods } from "../controllers/adoption.controller.js";
+import { methods as adoptionController } from "../controllers/adoption.controller.js";
 
 const router = Router();
 
-router.get("/");
-router.post("/post");
+router.get("/", adoptionController.getAdoption);
+router.post("/post", adoptionController.postAdoption);
 
 export default router;
