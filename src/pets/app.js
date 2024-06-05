d0d1a4ea7f89d72d0a76388b816c6b0ae726d7cb
ememendozar <ememendozar002@gmail.com>
2024-06-05 00:05:21 -0500

    Feat: Added swagger for pets

--- src/pets/app.js
+++ src/pets/app.js
@@ -1,15 +1,19 @@
-import express from "express";
 import petRoutes from "./routes/pet.route.js";
 import pictureRoutes from "./routes/picture.route.js";
 import medicalDataRoutes from "./routes/medical_data.route.js";
 import fileUpload from "express-fileupload";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import express from "express";
 
 const app = express();
+
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 app.use(
   fileUpload({
     useTempFiles: true,
-    temFileDir: "./uploads",
+    tempFileDir: "./uploads",
   })
 );
 
@@ -22,5 +26,6 @@ app.use(express.json());
 app.use("/api/pets", petRoutes);
 app.use("/api/pictures", pictureRoutes);
 app.use("/api/medicalData", medicalDataRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app;
